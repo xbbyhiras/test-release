07193bb641b156d8250398424f8f7a61b328ea69
Xavier Brinon <103182760+xbbyhiras@users.noreply.github.com>
2023-02-03 05:43:11 +0000

    create banner for admin app

--- repositoire/apps/admin/src/app/app.tsx
+++ repositoire/apps/admin/src/app/app.tsx
@@ -1,13 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
-import NxWelcome from './nx-welcome';
-
+import { Banner } from '@repositoire/common-ui'
 import { Route, Routes, Link } from 'react-router-dom';
 
 export function App() {
   return (
     <>
-      <NxWelcome title="admin" />
+      <Banner text="Welcome to our admin app." />
       <div />
 
       {/* START: routes */}
