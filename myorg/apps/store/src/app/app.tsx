f95e702c1033eb7b7a744021244189c5d927a449
Xavier Brinon <103182760+xbbyhiras@users.noreply.github.com>
2023-02-05 01:13:12 +0000

    commiting to test affected

---
+++ myorg/apps/store/src/app/app.tsx
@@ -0,0 +1,20 @@
+import { Banner } from '@myorg/common-ui';
+import { exampleProducts } from '@myorg/products';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function App() {
+  return (
+    <>
+      <Banner text="Welcome to the store!" />
+      <ul>
+        {exampleProducts.map((product) => (
+          <li key={product.id}>
+            <strong>{product.name}</strong> Price: {product.price}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default App;
